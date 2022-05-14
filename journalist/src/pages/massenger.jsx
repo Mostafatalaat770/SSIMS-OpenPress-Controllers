50cb0bcbff57257dd0e2c71a1e79464fa04d9f82
bishoyrefaat <48106126+bishoyrefaat@users.noreply.github.com>
2022-05-14 15:17:37 -0700

    journalist: add initlal messenger

---
+++ journalist/src/pages/massenger.jsx
@@ -0,0 +1,16 @@
+export const Massenger = () => {
+    return(
+
+        <><><div>
+            <h1>users</h1>
+        </div><div>
+            <h1>text</h1>
+        </div></><div>
+        <form name="message" action="">
+                <input name="usermsg" type="text" id="usermsg" />
+                <input name="submitmsg" type="submit" id="submitmsg" value="Send" />
+            </form>
+        </div></>
+       
+        );
+};
