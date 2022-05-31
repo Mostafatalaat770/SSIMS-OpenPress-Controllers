55ddfacafce67a2db29438b670dbc7d160bee8c6
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-06-01 01:31:52 +0200

    University: add connections page skeleton

---
+++ University/src/pages/connections.tsx
@@ -0,0 +1,43 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FC } from "../types";
+
+const navElements = [
+	{
+		name: "Active",
+		path: "/active",
+	},
+	{
+		name: "Pending",
+		path: "/pending",
+	},
+	{
+		name: "New",
+		path: "/new",
+	},
+	{
+		name: "Accept",
+		path: "/accept",
+	},
+];
+
+export const Connections: FC = () => {
+	return (
+		<div>
+			<Navbar bg="light" expand="lg">
+				<Container>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							{navElements.map((e) => (
+								<Nav.Link as={Link} key={e.path} to={e.path}>
+									{e.name}
+								</Nav.Link>
+							))}
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</div>
+	);
+};
--- University/src/router/app.tsx
+++ University/src/router/app.tsx
@@ -3,12 +3,14 @@ import { Home } from "../pages/home";
 import { Layout } from "./layout";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Connections } from "../pages/connections";
 
 export const AppRouter = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Layout />}>
 				<Route index element={<Home />} />
+				<Route path="/connections" element={<Connections />} />
 			</Route>
 		</Routes>
 	);
