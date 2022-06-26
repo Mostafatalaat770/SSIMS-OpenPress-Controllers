2d1b18c90d8a5cf7b776c7464be03326eb82bcb0
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-06-26 09:29:54 +0200

    University: simplify connections - only active connections are needed in case of university(multiple connections)

--- University/src/router/app.tsx
+++ University/src/router/app.tsx
@@ -5,9 +5,6 @@ import { Layout } from "./layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Connections } from "../pages/connections";
 import { Active } from "../pages/connections/active";
-import { Pending } from "../pages/connections/pending";
-import { NewConnection } from "../pages/connections/newConnection";
-import { AcceptConnection } from "../pages/connections/acceptConnection";
 import { Credentials } from "../pages/credentials";
 
 export const AppRouter = () => {
@@ -17,10 +14,6 @@ export const AppRouter = () => {
 				<Route index element={<Home />} />
 				<Route path="connections" element={<Connections />}>
 					<Route index element={<Active />} />
-					<Route path="active" element={<Active />} />
-					<Route path="pending" element={<Pending />} />
-					<Route path="new" element={<NewConnection />} />
-					<Route path="accept" element={<AcceptConnection />} />
 				</Route>
 				<Route path="credentials" element={<Credentials />} />
 			</Route>
