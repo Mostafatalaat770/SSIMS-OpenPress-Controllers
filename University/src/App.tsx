912e380d78a6c37d8c710b9d23c9580595396f09
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-06-29 01:06:25 +0200

    add dockerfile and nginx.conf

--- University/src/App.tsx
+++ University/src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { AppRouter } from "./router/app";
 import { FC } from "./types";
 export const App: FC = () => {
+	console.log(process.env.REACT_APP_UNIVERSITY_AGENT_HOST);
+
 	return (
 		<div className="App">
 			<AppRouter />
